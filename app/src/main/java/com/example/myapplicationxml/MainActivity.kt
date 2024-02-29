d51a0ff1e055a983bc58c31660a604a5ca382e66
akaRomal <adfsdf@gmail.com>
2024-02-29 20:14:55 +0600

    first commit

---
+++ app/src/main/java/com/example/myapplicationxml/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myapplicationxml
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplicationxml.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding!!.root)
+    }
+}
